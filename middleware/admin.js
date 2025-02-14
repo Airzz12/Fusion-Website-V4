232a76c7d5b8aba30d73e611b85240c5660dfc0a
Airzz12 <123503973+Airzz12@users.noreply.github.com>
2025-02-14 22:06:14 +0800

    fix: Role admin cannot acess admin pannel fixed

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,5 +1,5 @@
 function admin(req, res, next) {
-    if (!req.user || req.user.rank !== 'admin') {
+    if (!req.user || req.user.rank.toLowerCase() !== 'admin') {
         console.log('Unauthorized access attempt to admin panel');
         return res.status(403).redirect('/');
     }
